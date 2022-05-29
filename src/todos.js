09d94f9910270fdab7637ea5601ea22b3e52e0f0
Alin Ibadof <alinibadof@gmail.com>
2022-05-29 18:08:21 +0300

    fix new task structure, add minimalistic styling

--- src/todos.js
+++ src/todos.js
@@ -27,19 +27,47 @@ function displayTask() {
 
   const titleContainer = document.createElement("div");
   titleContainer.classList.add("tasktitle");
-  titleContainer.textContent = `Title: ${task.title}`;
+  const titleText = document.createElement("div");
+  titleText.textContent = `Title: `;
+  const titleWritten = document.createElement("div");
+  titleWritten.textContent = `${task.title}`;
+  titleText.classList.add("task-left");
+  titleWritten.classList.add("task-right");
+  titleContainer.appendChild(titleText);
+  titleContainer.appendChild(titleWritten);
 
   const descriptionContainer = document.createElement("div");
   descriptionContainer.classList.add("taskdescription");
-  descriptionContainer.textContent = `Description: ${task.description}`;
+  const descriptionText = document.createElement("div");
+  descriptionText.textContent = `Description: `;
+  const descriptionWritten = document.createElement("div");
+  descriptionWritten.textContent = `${task.description}`;
+  descriptionText.classList.add("task-left");
+  descriptionWritten.classList.add("task-right");
+  descriptionContainer.appendChild(descriptionText);
+  descriptionContainer.appendChild(descriptionWritten);
 
   const dateContainer = document.createElement("div");
   dateContainer.classList.add("taskduedate");
-  dateContainer.textContent = `Due date: ${task.date}`;
+  const dateText = document.createElement("div");
+  dateText.textContent = `Due Date: `;
+  const dateWritten = document.createElement("div");
+  dateWritten.textContent = `${task.date}`;
+  dateText.classList.add("task-left");
+  dateWritten.classList.add("task-right");
+  dateContainer.appendChild(dateText);
+  dateContainer.appendChild(dateWritten);
 
   const priorityContainer = document.createElement("div");
   priorityContainer.classList.add("taskpriority");
-  priorityContainer.textContent = `Priority: ${task.priority}`;
+  const priorityText = document.createElement("div");
+  priorityText.textContent = `Priority: `;
+  const priorityWritten = document.createElement("div");
+  priorityWritten.textContent = `${task.priority}`;
+  priorityText.classList.add("task-left");
+  priorityWritten.classList.add("task-right");
+  priorityContainer.appendChild(priorityText);
+  priorityContainer.appendChild(priorityWritten);
 
   const removeContainer = document.createElement("img");
   removeContainer.classList.add("taskremcontainer");
@@ -61,8 +89,10 @@ function displayTask() {
 
 export { displayTask, addTask };
 
-//move form creation to separate module
-//clean displaytask function
 //style created tasks
 //make title required
 //look into local storage
+
+// make task objects inside of an array;
+// when displayfunction is called, run a for loop through array, and display all
+// this will help when creating new projects.
